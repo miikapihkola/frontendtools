3f8445ec787e279360af11005b289c52aec07965
Miika Pihkola <miika.pihkola@student.lab.fi>
2024-10-17 11:17:26 +0300

    Harkka1, Not Found + hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PageNotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'hello', component: HelloWorldComponent },
+    { path: '', redirectTo: 'hello', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent },
+];
