294f41df9060a4af1488a36fcdf64476ab0b025b
Miika Pihkola <miika.pihkola@student.lab.fi>
2024-11-05 02:07:53 +0200

    HT1: Done

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-hello-world',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './hello-world.component.html',
   styleUrl: './hello-world.component.css'
 })
-export class HelloWorldComponent {
 
+export class HelloWorldComponent {
+  title: string = 'front-end-kurssi'; // ": string" ei ole pakollinen määritys
 }
+
