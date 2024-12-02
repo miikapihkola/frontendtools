40a56bd7cce220bfe01b075e9b8e5914e4ab46c8
Miika Pihkola <miika.pihkola@student.lab.fi>
2024-12-02 13:52:42 +0200

    HT8: Done

--- src/app/cinema/cinema.component.ts
+++ src/app/cinema/cinema.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CinemaService } from '../services/cinema.service';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { Observable } from 'rxjs';
@@ -6,16 +7,20 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-cinema',
   standalone: true,
-  imports: [MatExpansionModule],
+  imports: [MatExpansionModule, CommonModule],
   templateUrl: './cinema.component.html',
   styleUrl: './cinema.component.css'
 })
 export class CinemaComponent {
 
-  //private data$: Observable<any>;
-  private data: any;
+  data$: Observable<any>;
 
   constructor(private cinemaService: CinemaService) {
-    this.data = this.cinemaService.getTestData();
+    this.data$ = new Observable<any>();
+  }
+
+  ngOnInit(): void {
+    this.data$ = this.cinemaService.getData();
+    //console.log(this.data$)
   }
 }
--- src/app/services/cinema.service.ts
+++ src/app/services/cinema.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { parseString } from 'xml2js';
 
 @Injectable({
@@ -14,10 +14,20 @@ export class CinemaService {
   testData = [{ 'Title': 'Ensi-ilta: PalmSprings', 'PublishDate': '2020-11-24T00:00:00', 'HTMLLead': '', 'ImageURL': 'http://media.finnkino.fi/1012/news/6124/PalmSprings_550.jpg' }, { 'Title': 'Ensi-ilta: Karjamäenjoulu', 'PublishDate': '2020-11-24T00:00:00', 'HTMLLead': '', 'ImageURL': 'http://media.finnkino.fi/1012/news/6123/JulPaKutoppen_550.jpg' }];
 
   getData(): Observable<any> {
-    return this.httpClient.get(this.url, { responseType: "text" }).pipe(map(response => { console.log("get xml data..." + response); let data: any; parseString(response, { trim: true, explicitArray: false, mergeAttrs: true }, function (err: any, result: any) { data = result.News.Newsarticle }) }))
+    return this.httpClient.get(this.url, { responseType: "text" })
+      .pipe(map(response => {
+        //console.log("get xml data..." + response);
+        let newsData: any;
+        parseString(response, { trim: true, explicitArray: false, mergeAttrs: true },
+          function (err: any, result: any) {
+            newsData = result.News.NewsArticle;
+            //console.log(JSON.stringify(newsData));
+          });
+        return newsData;
+      }))
   }
 
   getTestData() {
-    return this.testData
+    return of(this.testData)
   }
 }
